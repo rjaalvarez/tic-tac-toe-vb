e380931e44e4230b71ea4dc95406f3f545036bd6
iamrja <rj123alv@gmail.com>
2022-05-22 10:08:38 +0800

    added UI, newGame, exit, winner for X

---
+++ Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class tictactoeVB
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HowToPlayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.topLeftButton = New System.Windows.Forms.Button()
+        Me.topMidButton = New System.Windows.Forms.Button()
+        Me.topRightButton = New System.Windows.Forms.Button()
+        Me.leftMidButton = New System.Windows.Forms.Button()
+        Me.midButton = New System.Windows.Forms.Button()
+        Me.rightMidButton = New System.Windows.Forms.Button()
+        Me.botLeftButton = New System.Windows.Forms.Button()
+        Me.botMidButton = New System.Windows.Forms.Button()
+        Me.botRightButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(507, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewGameToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'NewGameToolStripMenuItem
+        '
+        Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
+        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewGameToolStripMenuItem.Text = "New game"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HowToPlayToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'HowToPlayToolStripMenuItem
+        '
+        Me.HowToPlayToolStripMenuItem.Name = "HowToPlayToolStripMenuItem"
+        Me.HowToPlayToolStripMenuItem.Size = New System.Drawing.Size(138, 22)
+        Me.HowToPlayToolStripMenuItem.Text = "How to play"
+        '
+        'topLeftButton
+        '
+        Me.topLeftButton.Location = New System.Drawing.Point(116, 131)
+        Me.topLeftButton.Name = "topLeftButton"
+        Me.topLeftButton.Size = New System.Drawing.Size(75, 75)
+        Me.topLeftButton.TabIndex = 1
+        Me.topLeftButton.TabStop = False
+        Me.topLeftButton.UseVisualStyleBackColor = True
+        '
+        'topMidButton
+        '
+        Me.topMidButton.Location = New System.Drawing.Point(207, 131)
+        Me.topMidButton.Name = "topMidButton"
+        Me.topMidButton.Size = New System.Drawing.Size(75, 75)
+        Me.topMidButton.TabIndex = 2
+        Me.topMidButton.UseVisualStyleBackColor = True
+        '
+        'topRightButton
+        '
+        Me.topRightButton.Location = New System.Drawing.Point(299, 131)
+        Me.topRightButton.Name = "topRightButton"
+        Me.topRightButton.Size = New System.Drawing.Size(75, 75)
+        Me.topRightButton.TabIndex = 3
+        Me.topRightButton.UseVisualStyleBackColor = True
+        '
+        'leftMidButton
+        '
+        Me.leftMidButton.Location = New System.Drawing.Point(116, 222)
+        Me.leftMidButton.Name = "leftMidButton"
+        Me.leftMidButton.Size = New System.Drawing.Size(75, 75)
+        Me.leftMidButton.TabIndex = 4
+        Me.leftMidButton.UseVisualStyleBackColor = True
+        '
+        'midButton
+        '
+        Me.midButton.Location = New System.Drawing.Point(207, 222)
+        Me.midButton.Name = "midButton"
+        Me.midButton.Size = New System.Drawing.Size(75, 75)
+        Me.midButton.TabIndex = 5
+        Me.midButton.UseVisualStyleBackColor = True
+        '
+        'rightMidButton
+        '
+        Me.rightMidButton.Location = New System.Drawing.Point(299, 222)
+        Me.rightMidButton.Name = "rightMidButton"
+        Me.rightMidButton.Size = New System.Drawing.Size(75, 75)
+        Me.rightMidButton.TabIndex = 6
+        Me.rightMidButton.UseVisualStyleBackColor = True
+        '
+        'botLeftButton
+        '
+        Me.botLeftButton.Location = New System.Drawing.Point(116, 312)
+        Me.botLeftButton.Name = "botLeftButton"
+        Me.botLeftButton.Size = New System.Drawing.Size(75, 75)
+        Me.botLeftButton.TabIndex = 7
+        Me.botLeftButton.UseVisualStyleBackColor = True
+        '
+        'botMidButton
+        '
+        Me.botMidButton.Location = New System.Drawing.Point(207, 312)
+        Me.botMidButton.Name = "botMidButton"
+        Me.botMidButton.Size = New System.Drawing.Size(75, 75)
+        Me.botMidButton.TabIndex = 8
+        Me.botMidButton.UseVisualStyleBackColor = True
+        '
+        'botRightButton
+        '
+        Me.botRightButton.Location = New System.Drawing.Point(299, 312)
+        Me.botRightButton.Name = "botRightButton"
+        Me.botRightButton.Size = New System.Drawing.Size(75, 75)
+        Me.botRightButton.TabIndex = 9
+        Me.botRightButton.UseVisualStyleBackColor = True
+        '
+        'tictactoeVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 442)
+        Me.Controls.Add(Me.botRightButton)
+        Me.Controls.Add(Me.botMidButton)
+        Me.Controls.Add(Me.botLeftButton)
+        Me.Controls.Add(Me.rightMidButton)
+        Me.Controls.Add(Me.midButton)
+        Me.Controls.Add(Me.leftMidButton)
+        Me.Controls.Add(Me.topRightButton)
+        Me.Controls.Add(Me.topMidButton)
+        Me.Controls.Add(Me.topLeftButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "tictactoeVB"
+        Me.Text = "Tic Tac Toe"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewGameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HowToPlayToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents topLeftButton As Button
+    Friend WithEvents topMidButton As Button
+    Friend WithEvents topRightButton As Button
+    Friend WithEvents leftMidButton As Button
+    Friend WithEvents midButton As Button
+    Friend WithEvents rightMidButton As Button
+    Friend WithEvents botLeftButton As Button
+    Friend WithEvents botMidButton As Button
+    Friend WithEvents botRightButton As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class tictactoeVB
+    Dim playturn As Boolean = True
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
+        topLeftButton.Enabled = True
+        topMidButton.Enabled = True
+        topRightButton.Enabled = True
+        leftMidButton.Enabled = True
+        midButton.Enabled = True
+        rightMidButton.Enabled = True
+        botLeftButton.Enabled = True
+        botMidButton.Enabled = True
+        botRightButton.Enabled = True
+
+        topLeftButton.Text = ""
+        topMidButton.Text = ""
+        topRightButton.Text = ""
+        leftMidButton.Text = ""
+        midButton.Text = ""
+        rightMidButton.Text = ""
+        botLeftButton.Text = ""
+        botMidButton.Text = ""
+        botRightButton.Text = ""
+    End Sub
+
+    Private Sub buttonFalse()
+        topLeftButton.Enabled = False
+        topMidButton.Enabled = False
+        topRightButton.Enabled = False
+        leftMidButton.Enabled = False
+        midButton.Enabled = False
+        rightMidButton.Enabled = False
+        botLeftButton.Enabled = False
+        botMidButton.Enabled = False
+        botRightButton.Enabled = False
+    End Sub
+
+    Private Sub checkWinner()
+        If topLeftButton.Text = "X" And topMidButton.Text = "X" And topRightButton.Text = "X" Then
+            topLeftButton.BackColor = System.Drawing.Color.White
+            topMidButton.BackColor = System.Drawing.Color.White
+            topRightButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If topLeftButton.Text = "X" And midButton.Text = "X" And botRightButton.Text = "X" Then
+            topLeftButton.BackColor = System.Drawing.Color.White
+            midButton.BackColor = System.Drawing.Color.White
+            botRightButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If topLeftButton.Text = "X" And leftMidButton.Text = "X" And botLeftButton.Text = "X" Then
+            topLeftButton.BackColor = System.Drawing.Color.White
+            leftMidButton.BackColor = System.Drawing.Color.White
+            botLeftButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If leftMidButton.Text = "X" And midButton.Text = "X" And rightMidButton.Text = "X" Then
+            leftMidButton.BackColor = System.Drawing.Color.White
+            midButton.BackColor = System.Drawing.Color.White
+            rightMidButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If topMidButton.Text = "X" And midButton.Text = "X" And botMidButton.Text = "X" Then
+            topMidButton.BackColor = System.Drawing.Color.White
+            midButton.BackColor = System.Drawing.Color.White
+            botMidButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If botLeftButton.Text = "X" And midButton.Text = "X" And topRightButton.Text = "X" Then
+            botLeftButton.BackColor = System.Drawing.Color.White
+            midButton.BackColor = System.Drawing.Color.White
+            topRightButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If botLeftButton.Text = "X" And botMidButton.Text = "X" And botRightButton.Text = "X" Then
+            botLeftButton.BackColor = System.Drawing.Color.White
+            botMidButton.BackColor = System.Drawing.Color.White
+            botRightButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+
+        If topRightButton.Text = "X" And rightMidButton.Text = "X" And botRightButton.Text = "X" Then
+            topRightButton.BackColor = System.Drawing.Color.White
+            rightMidButton.BackColor = System.Drawing.Color.White
+            botRightButton.BackColor = System.Drawing.Color.White
+            MessageBox.Show("X is the Winner!")
+            buttonFalse()
+        End If
+    End Sub
+End Class
